ad399781ad969cf32644635b178207d75e4e4841
haikalimamridha <haikalimamridha@gmail.com>
2026-09-24 14:55:52 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,46 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservasiController;
+use App\Http\Controllers\AuthController;
+use App\Models\Facility;
 
 Route::get('/', function () {
-    return view('fasilitas');
+    $facilities = Facility::limit(10)->get();
+
+    return view('fasilitas', compact('facilities'));
 })->name('fasilitas');
 
 Route::get('/fasilitas', function () {
-    return view('fasilitas');
+    $facilities = Facility::limit(10)->get();
+
+    return view('fasilitas', compact('facilities'));
 })->name('fasilitas');
 
+// Pengguna - Login
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
+Route::get('/login', [AuthController::class, 'showLogin'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.process');
+
+// Pengguna - Registrasi
 Route::get('/register', function () {
     return view('auth.register');
 })->name('register');
+
+Route::get('/register', [AuthController::class, 'showRegister'])
+    ->name('register');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('register.process');
+
+//logout
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
 
 // Pengguna — reservasi
 Route::get('/riwayat-reservasi', function () {
